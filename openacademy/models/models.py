9395997a9a6bcaf7d31fe08732e0303e2bd62e8c
paulplazaa <g.plaza@jardinazuayo.fin.ec>
2020-08-12 14:11:15 -0500

    Herencia

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -8,7 +8,7 @@ class Course(models.Model):
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
-    responsable_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
+    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
     session_ids = fields.One2many('openacademy.session', 'course_id')
 
 
