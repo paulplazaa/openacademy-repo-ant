096da7d800ba97ceb46db1a485c0dd4292780089
paulplazaa <g.plaza@jardinazuayo.fin.ec>
2020-08-12 12:44:03 -0500

    Relaciones Many2many, One2many y Herencia

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from odoo import models, fields, api
 
 class Course(models.Model):
@@ -6,3 +8,22 @@ class Course(models.Model):
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+    responsable_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many('openacademy.session', 'course_id')
+
+
+
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+    _description = "openacademy session of test"
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor", domain=['|', ('instructor','=',True), ('category_id.name','ilike','Teacher')])
+    course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
